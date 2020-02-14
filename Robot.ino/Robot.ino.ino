692649bc0f5bb3aa76cd5ff0f433f647fd9941f4
zach42o1 <48109689+zach42o1@users.noreply.github.com>
2020-02-14 12:32:41 -0700

    Third Commit

---
+++ Robot.ino/Robot.ino.ino
@@ -0,0 +1,45 @@
+#include <Servo.h>    //LIbrary for the Servo
+Servo Left_Arm;       //Assign Servo variable Left_Arm
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+  Left_Arm.attach(3); //Attaches Servo to PIN 3
+
+}
+
+void loop() {
+
+
+}
+//****************SERVO FUNCTIONS*************************
+// Use this for the functionality of the servos 
+//********************************************************
+
+
+//*********************WAVE*********************************
+// This is just a function to test the movement of the servo
+//**********************************************************
+void Wave() 
+{
+  Left_Arm.write(40); //moves the arm to 40 degree
+  delay(3000);        // 3second pause
+  Left_Arm.write(270);//moves the arm to 270 degree
+  delay(3000);        // 3 second pause
+  Left_Arm.write(40); //moves the arm to 40 degree
+}
+
+//*********************PICKMYSELFUP*************************
+// This allows the robot to pick itself back up when resting 
+// the Front Bumper
+//**********************************************************
+void PickMyselfUp() 
+{
+  Left_Arm.write(270); //moves the arm to 270 degree
+  delay(1000);         // 1 second delay
+  Left_Arm.write(40);  //moves the arm to 40 degrees
+  delay(200);         // 2 second delay
+  Left_Arm.write(180); //points arm outward
+  
+}
